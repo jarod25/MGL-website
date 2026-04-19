6fbbab6d406e5ebc9b61eacd19d79ec867f5c58f
KOHLER Jarod <kohler.jarod.2004@gmail.com>
2026-04-19 15:49:11 +0200

    Add configuration files for property info and CSRF protection; update dependencies to Symfony 7.4

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -177,8 +177,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return (string)$this->email;
     }
 
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
-
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
