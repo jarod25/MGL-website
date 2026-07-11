6d7b54850f1d7fe413e1967c32c17c5f3f1cefbe
KOHLER Jarod <kohler.jarod.2004@gmail.com>
2026-07-11 13:58:39 +0200

    Remove "Mulhouse Alsace Agglomération" partner references, unused styles, and update organizer data

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -25,12 +25,6 @@ final class AboutController extends AbstractController
                 'url' => 'https://www.crous-strasbourg.fr/',
             ],
             [
-                'key' => 'mulhouse_alsace_agglomeration',
-                'name' => 'Mulhouse Alsace Agglomération',
-                'logoPath' => '/images/partners/mulhouse-alsace-agglomeration-logo.png',
-                'url' => 'https://www.m2a.fr/',
-            ],
-            [
                 'key' => 'universite_haute_alsace',
                 'name' => 'Université de Haute-Alsace',
                 'logoPath' => '/images/partners/universite-haute-alsace-logo.png',
@@ -56,7 +50,64 @@ final class AboutController extends AbstractController
             ],
         ];
 
-        $organizers = [];
+        $organizers = [
+            [
+                'key' => 'raphael',
+                'firstname' => 'Raphaël',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+                'photoPath' => '/images/team/raphael.jpg',
+            ],
+            [
+                'key' => 'alex',
+                'firstname' => 'Alex',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+//                'photoPath' => '/images/team/alex.jpg',
+            ],
+            [
+                'key' => 'dominic',
+                'firstname' => 'Dominic',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+//                'photoPath' => '/images/team/dominic.jpg',
+            ],
+            [
+                'key' => 'loic',
+                'firstname' => 'Loïc',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+//                'photoPath' => '/images/team/loic.jpg',
+            ],
+            [
+                'key' => 'martin',
+                'firstname' => 'Martin',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+//                'photoPath' => '/images/team/martin.jpg',
+            ],
+            [
+                'key' => 'nathan',
+                'firstname' => 'Nathan',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+                'photoPath' => '/images/team/nathan.jpg',
+            ],
+            [
+                'key' => 'unknown',
+                'firstname' => 'Unknown',
+                'role' => 'Organisateur',
+                'affiliation' => 'ENSCMu',
+//                'photoPath' => '/images/team/unknown.jpg',
+            ],
+            [
+                'key' => 'jarod',
+                'firstname' => 'Jarod',
+                'role' => 'Développeur',
+                'affiliation' => 'ENSISA / XID',
+                'photoPath' => '/images/team/jarod.jpg',
+            ],
+        ];
 
         return $this->render('about/index.html.twig', [
             'partners' => $partners,
